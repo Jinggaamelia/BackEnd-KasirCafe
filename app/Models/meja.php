43291932f5a9b6eef8bc2fce218f7f4b3d8fa359
Jinggaamelia <jinggaa84@gmail.com>
2023-08-24 07:34:06 +0700

    new commit

---
+++ app/Models/meja.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class meja extends Model
+{
+    use HasFactory;
+    public $timestamps= null;
+    protected $table="mejas";
+    protected $primarykey="id_meja";
+    protected $fillable=['nomor_meja','status'];
+}
